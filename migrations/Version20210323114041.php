daef0cff5dcccb0da3526efd9b20e7965f5a1811
ocnelias <ocnelias@gmail.com>
2021-03-25 09:07:43 +0200

    fix

--- migrations/Version20210323114041.php
+++ migrations/Version20210323114041.php
@@ -21,7 +21,7 @@ final class Version20210323114041 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE details (id INT AUTO_INCREMENT NOT NULL, query_id INT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, INDEX IDX_72260B8AEF946F99 (query_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE query (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, date_time DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE query (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, date_time DATETIME, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE details ADD CONSTRAINT FK_72260B8AEF946F99 FOREIGN KEY (query_id) REFERENCES query (id)');
     }
 
